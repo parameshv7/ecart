4567b49e1a70c6180f7b6f905970bd16ea7ca052
Parameshwaran Venkatesan <parameshvenkat98@gmail.com>
2022-09-29 12:19:26 +0530

    ur

--- src/Home.tsx
+++ src/Home.tsx
@@ -24,7 +24,7 @@ function Home() {
   }, []);
 
   const gettodos = async () => {
-    const response = await axios.get(`http://localhost:5001/todos`);
+    const response = await axios.get(`http://localhost:1000/todos`);
     console.log(response.data);
     setTodos(response.data);
   };
